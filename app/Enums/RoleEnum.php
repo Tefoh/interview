9e5a3b3292a9a66b9efa315b169b246ab77968f1
Tefoh <tefo.ha27@gmail.com>
2023-12-21 22:30:57 +0330

    feat(admin): add role column and enum to verify the users and admin

---
+++ app/Enums/RoleEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum RoleEnum: int
+{
+    case USER = 1;
+    case ADMIN = 2;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\RoleEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -41,5 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'role' => RoleEnum::class,
     ];
+
+    public function isAdmin(): bool
+    {
+        return $this->role === RoleEnum::ADMIN;
+    }
 }
