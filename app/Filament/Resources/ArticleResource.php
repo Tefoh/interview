dfe20fe1fa0ef78f28bcb10cfd49009ee3229b08
Tefoh <tefo.ha27@gmail.com>
2023-12-22 17:33:38 +0330

    WIP(article): add view and delete action

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -107,7 +107,9 @@ class ArticleResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
